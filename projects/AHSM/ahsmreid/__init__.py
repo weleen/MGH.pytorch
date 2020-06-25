ada9f325abc7c3fd6157b9094b44da7ffaf937fc
WenhuZhang <358607757@qq.com>
2020-06-25 22:17:10 +0800

    Upload New File

---
+++ projects/AHSM/ahsmreid/__init__.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+"""
+@author:  wenhuzhang
+@contact: Andrew-pph@outlook.com
+"""
+from .config import add_ahsmreid_config
+from .ahsm_trainer import AHSMTrainer
+from fastreid.engine import hooks
