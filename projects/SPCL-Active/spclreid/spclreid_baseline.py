36a7515d940194934ecb6c3adc02e8b2601d1180
tianjian <tianjian29@zju.edu.cn>
2020-07-30 12:09:48 +0800

    fix bug in spcl+active

--- projects/SPCL-Active/spclreid/spclreid_baseline.py
+++ projects/SPCL-Active/spclreid/spclreid_baseline.py
@@ -50,8 +50,7 @@ class USL_Baseline(nn.Module):
     def forward(self, batched_inputs):
         if not self.training:
             pred_feat = self.inference(batched_inputs)
-            try:              return pred_feat, batched_inputs["targets"], batched_inputs["camid"]
-            except Exception: return pred_feat
+            return pred_feat
 
         images = self.preprocess_image(batched_inputs)
         targets = batched_inputs["targets"].long()
