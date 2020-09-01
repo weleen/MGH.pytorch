82a57d22fa892713d65130e5a1c12b3a78317a93
WuYiming <yimingwu@hotmail.com>
2020-09-01 22:51:15 +0800

    1. Remove useless optimizer. 2. Update NUM_WORKERS and CHECKPOINT_PERIOD in yml file

--- fastreid/solver/optim/__init__.py
+++ fastreid/solver/optim/__init__.py
@@ -1,11 +1,4 @@
 from .lamb import Lamb
-from .lars import LARS
-from .lookahead import Lookahead, LookaheadAdam
-from .novograd import Novograd
-from .over9000 import Over9000, RangerLars
-from .radam import RAdam, PlainRAdam, AdamW
-from .ralamb import Ralamb
-from .ranger import Ranger
 from .swa import SWA
 
 from torch.optim import *
--- projects/PartialReID/train_net.py
+++ projects/PartialReID/train_net.py
@@ -1,19 +1,18 @@
+#!/usr/bin/env python
 # encoding: utf-8
 """
 @author:  sherlock
 @contact: sherlockliao01@gmail.com
 """
 
-import os
 import logging
+import os
 import sys
 
 sys.path.append('.')
 
-from torch import nn
-
-from fastreid.config import get_cfg
-from fastreid.engine import DefaultTrainer, default_argument_parser, default_setup
+from fastreid.config import cfg
+from fastreid.engine import DefaultTrainer, default_argument_parser, default_setup, launch
 from fastreid.utils.checkpoint import Checkpointer
 from fastreid.engine import hooks
 
@@ -32,7 +31,6 @@ def setup(args):
     """
     Create configs and perform basic setups.
     """
-    cfg = get_cfg()
     add_partialreid_config(cfg)
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
@@ -44,15 +42,14 @@ def setup(args):
 def main(args):
     cfg = setup(args)
 
-    logger = logging.getLogger('fastreid.' + __name__)
     if args.eval_only:
+        logger = logging.getLogger("fastreid.trainer")
         cfg.defrost()
         cfg.MODEL.BACKBONE.PRETRAIN = False
         model = Trainer.build_model(cfg)
-        model = nn.DataParallel(model)
-        model = model.cuda()
 
-        Checkpointer(model, save_dir=cfg.OUTPUT_DIR).load(cfg.MODEL.WEIGHTS)  # load trained model
+        Checkpointer(model).load(cfg.MODEL.WEIGHTS)  # load trained model
+
         if cfg.TEST.PRECISE_BN.ENABLED and hooks.get_bn_modules(model):
             prebn_cfg = cfg.clone()
             prebn_cfg.DATALOADER.NUM_WORKERS = 0  # save some memory and time for PreciseBN
@@ -76,4 +73,11 @@ def main(args):
 if __name__ == "__main__":
     args = default_argument_parser().parse_args()
     print("Command Line Args:", args)
-    main(args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
